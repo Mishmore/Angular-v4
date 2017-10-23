28b3bd4951334de8923eadb2cbd81b990edad5a6
Mishmore <michellemore28@gmail.com>
2017-10-23 18:41:49 +0300

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Platzi Square';
   lugares:any = [
-    {nombre: 'Polleria'},
-    {nombre: 'Panaderia'},
-    {nombre: 'Cevicheria'},    
+    {active: true, nombre: 'Polleria'},
+    {active: false, nombre: 'Panaderia'},
+    {active: true, nombre: 'Cevicheria'},    
   ];
+  lat:number = 37.977503;
+  lng:number = 23.7238216;
+
 
   constructor() {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAdxVDrNzeEHwCahuVQQ7Jovhun60AvXBo'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
