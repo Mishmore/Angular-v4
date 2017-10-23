dca436ba45372cf39523acc8b6cf5ab7a544fa9d
Mishmore <michellemore28@gmail.com>
2017-10-23 18:51:39 +0300

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,8 @@ export class AppComponent {
   lugares:any = [
     {active: true, nombre: 'Polleria'},
     {active: false, nombre: 'Panaderia'},
-    {active: true, nombre: 'Cevicheria'},    
+    {active: true, nombre: 'Cevicheria'},  
+    {active: true, nombre: 'Parrilleria'},    
   ];
   lat:number = 37.977503;
   lng:number = 23.7238216;
